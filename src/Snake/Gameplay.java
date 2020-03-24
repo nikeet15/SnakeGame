ed576066e05dd64f27a6974e59e35e7bc4620a1c
nikeet15 <kesharinikeet@gmail.com>
2020-03-25 00:40:33 +0530

    snake UI created

--- src/Snake/Gameplay.java
+++ src/Snake/Gameplay.java
@@ -7,18 +7,47 @@ package Snake;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  *
  * @author acer
  */
-public class Gameplay extends JPanel{
+public class Gameplay extends JPanel implements KeyListener, ActionListener{
  
     private ImageIcon t;
+    private ImageIcon leftmouth;
+    private ImageIcon rightmouth; 
+    private ImageIcon upmouth;
+    private ImageIcon downmouth;
+    private ImageIcon snakeimage;
+    
+    private int[] snakeXlength= new int[750];   //at 0th index head of snake
+    private int[] snakeYlength= new int[750];
+    private int len= 3;
+    
+    private boolean left= false;
+    private boolean right= false;
+    private boolean up= false;
+    private boolean down= false;
+    
+    private Timer timer;
+    private int delay= 100;
+    private int mov=0;
     
     public Gameplay(){
+        
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer= new Timer(delay, this);
+        timer.start();
         
     }
    
@@ -41,8 +70,87 @@ public class Gameplay extends JPanel{
         g.setColor(Color.black);
         g.fillRect(25,75,850,575);
         
+        //initialization of snake................
+        
+        //at start putting snake at left corner
+        if(mov==0)
+        {
+            snakeXlength[2]= 50;
+            snakeXlength[1]= 75;
+            snakeXlength[0]= 100;
+            
+            snakeYlength[2]= 100;
+            snakeYlength[1]= 100;
+            snakeYlength[0]= 100;
+        }
+        
+        rightmouth= new ImageIcon("Icons\\rightmouth.png");
+        rightmouth.paintIcon(this, g, snakeXlength[0], snakeYlength[0]);
+        
+        for (int i = 0; i < len; i++) {
+            if(i==0)
+            {
+                if(right)
+                {   rightmouth= new ImageIcon("Icons\\rightmouth.png");
+                    rightmouth.paintIcon(this, g, snakeXlength[i], snakeYlength[i]);
+                }
+            
+                if(left)
+                {
+                    leftmouth= new ImageIcon("Icons\\leftmouth.png");
+                    leftmouth.paintIcon(this, g, snakeXlength[i], snakeYlength[i]);
+                }
+            
+                if(up)
+                {   
+                    upmouth= new ImageIcon("Icons\\upmouth.png");
+                    upmouth.paintIcon(this, g, snakeXlength[i], snakeYlength[i]);
+                }
+            
+                if(down)
+                {
+                    downmouth= new ImageIcon("Icons\\downmouth.png");
+                    downmouth.paintIcon(this, g, snakeXlength[i], snakeYlength[i]);
+                }
+            }
+            
+            else if(i!=0)
+            {   
+                snakeimage= new ImageIcon("Icons\\snakeimage.png");
+                snakeimage.paintIcon(this, g, snakeXlength[i], snakeYlength[i]);
+            }
+            
+            
+            
+        }
         
         
+        
+        g.dispose();
+        
+        
+        
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
